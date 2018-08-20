2c381ebebed213c33f592806006daf914e0e948c
imaduddin786 <shaikhkhoobtar55@gmail.com>
2018-08-20 09:43:41 +0530

    add all my files

---
+++ history.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+##---(Mon Aug  6 09:41:45 2018)---
+runfile('E:/prajwal/2158089/snake.py', wdir='E:/prajwal/2158089')
+
+##---(Mon Aug 20 09:06:22 2018)---
+runfile('C:/Users/CSE/.spyder-py3/temp.py', wdir='C:/Users/CSE/.spyder-py3')
